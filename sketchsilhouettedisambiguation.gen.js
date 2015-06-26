1f49e33500d1639e63f9f81331e135539d1eb752
mbartlett21 <mjmouse9999@gmail.com>
2015-06-26 16:18:55 +0000

    156.0

---
+++ sketchsilhouettedisambiguation.gen.js
@@ -0,0 +1,16 @@
+FeatureScript 146;
+/* Automatically generated file -- DO NOT EDIT */
+
+export enum SketchSilhouetteDisambiguation
+{
+    NONE,
+    ALIGNED,
+    ANTI_ALIGNED,
+    OUTER,
+    INNER,
+    TORUS_TOP,
+    TORUS_BOTTOM,
+    TORUS_LEFT,
+    TORUS_RIGHT
+}
+
