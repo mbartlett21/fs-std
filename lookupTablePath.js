53b7801a546caa48b7ffdfe010edb8279107e725
mbartlett21 <mjmouse9999@gmail.com>
2017-03-02 21:18:49 +0000

    531.0 normalise

--- lookupTablePath.js
+++ lookupTablePath.js
@@ -1,7 +1,7 @@
-FeatureScript 531; /* Automatically generated version */
-import(path : "onshape/std/math.fs", version : "531.0");
-import(path : "onshape/std/string.fs", version : "531.0");
-import(path : "onshape/std/units.fs", version : "531.0");
+FeatureScript 9999; /* Automatically generated version */
+import(path : "onshape/std/math.fs", version : "");
+import(path : "onshape/std/string.fs", version : "");
+import(path : "onshape/std/units.fs", version : "");
 
 /**
  * A `LookupTablePath` identifies a map of keys into a multi-level table.
--- partStudio.js
+++ partStudio.js
@@ -1,9 +1,9 @@
-FeatureScript 531; /* Automatically generated version */
-import(path : "onshape/std/context.fs", version : "531.0");
-import(path : "onshape/std/defaultFeatures.fs", version : "531.0");
+FeatureScript 9999; /* Automatically generated version */
+import(path : "onshape/std/context.fs", version : "");
+import(path : "onshape/std/defaultFeatures.fs", version : "");
 
-import(path : "onshape/std/containers.fs", version : "531.0");
-import(path : "onshape/std/units.fs", version : "531.0");
+import(path : "onshape/std/containers.fs", version : "");
+import(path : "onshape/std/units.fs", version : "");
 
 /** @internal */
 export function definePartStudio(partStudio is function, defaultLengthUnit is ValueWithUnits, defaults is map) returns function
