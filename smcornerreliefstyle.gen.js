6f866fa7c764d0496481eda097eba2cf2f8e7807
mbartlett21 <mjmouse9999@gmail.com>
2017-09-08 18:58:35 +0000

    675.0

--- smcornerreliefstyle.gen.js
+++ smcornerreliefstyle.gen.js
@@ -8,11 +8,16 @@ FeatureScript 9999; /* Automatically generated version */
 /**
  * @internal.
  * Types of corner relief in sheet metal parts.
- */
+ * Is only ever used in FeatureScript and no built-ins work with it.
+ * See [SMReliefStyle] for the enum used in built-ins */
 export enum SMCornerReliefStyle
 {
+    annotation {"Name" : "Sized rectangle"}
+    SIZED_RECTANGLE,
     annotation {"Name" : "Rectangle"}
     RECTANGLE,
+    annotation {"Name" : "Sized round"}
+    SIZED_ROUND,
     annotation {"Name" : "Round"}
     ROUND,
     annotation {"Name" : "Closed"}
