4728d9702447895c93261e92c1d55e751952753f
mbartlett21 <mjmouse9999@gmail.com>
2018-05-24 19:05:50 +0000

    834.0

--- entityinferencetype.gen.js
+++ entityinferencetype.gen.js
@@ -1,4 +1,4 @@
-FeatureScript 9999; /* Automatically generated version */
+FeatureScript 834; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
@@ -20,6 +20,7 @@ export enum EntityInferenceType
     BOTTOM_AXIS_POINT,
     ORIGIN_X,
     ORIGIN_Y,
-    ORIGIN_Z
+    ORIGIN_Z,
+    LOOP_CENTER
 }
 
