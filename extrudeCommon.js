a4a0ccef0449285579e9a8704738beaf4c3c42e1
mbartlett21 <mjmouse9999@gmail.com>
2018-11-09 21:58:41 +0000

    951.0 normalise

--- extrudeCommon.js
+++ extrudeCommon.js
@@ -1,18 +1,18 @@
-FeatureScript 951; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
 
-export import(path : "onshape/std/boundingtype.gen.fs", version : "951.0");
+export import(path : "onshape/std/boundingtype.gen.fs", version : "");
 
-import(path : "onshape/std/curveGeometry.fs", version : "951.0");
-import(path : "onshape/std/evaluate.fs", version : "951.0");
-import(path : "onshape/std/feature.fs", version : "951.0");
-import(path : "onshape/std/manipulator.fs", version : "951.0");
-import(path : "onshape/std/query.fs", version : "951.0");
-import(path : "onshape/std/surfaceGeometry.fs", version : "951.0");
-import(path : "onshape/std/valueBounds.fs", version : "951.0");
-import(path : "onshape/std/vector.fs", version : "951.0");
+import(path : "onshape/std/curveGeometry.fs", version : "");
+import(path : "onshape/std/evaluate.fs", version : "");
+import(path : "onshape/std/feature.fs", version : "");
+import(path : "onshape/std/manipulator.fs", version : "");
+import(path : "onshape/std/query.fs", version : "");
+import(path : "onshape/std/surfaceGeometry.fs", version : "");
+import(path : "onshape/std/valueBounds.fs", version : "");
+import(path : "onshape/std/vector.fs", version : "");
 
 /**
  * Similar to `BoundingType`, but made for the second direction of an `extrude`.
