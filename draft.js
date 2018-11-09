f7041aab8ac10761a0ab4af9fbbe22d421cf149e
mbartlett21 <mjmouse9999@gmail.com>
2018-11-09 21:58:41 +0000

    951.0

--- draft.js
+++ draft.js
@@ -1,21 +1,21 @@
-FeatureScript 9999; /* Automatically generated version */
+FeatureScript 951; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
 
 // Imports used in interface
-export import(path : "onshape/std/query.fs", version : "");
+export import(path : "onshape/std/query.fs", version : "951.0");
 
 // Imports used internally
-import(path : "onshape/std/containers.fs", version : "");
-import(path : "onshape/std/curveGeometry.fs", version : "");
-import(path : "onshape/std/drafttype.gen.fs", version : "");
-import(path : "onshape/std/evaluate.fs", version : "");
-import(path : "onshape/std/feature.fs", version : "");
-import(path : "onshape/std/manipulator.fs", version : "");
-import(path : "onshape/std/topologyUtils.fs", version : "");
-import(path : "onshape/std/valueBounds.fs", version : "");
-import(path : "onshape/std/vector.fs", version : "");
+import(path : "onshape/std/containers.fs", version : "951.0");
+import(path : "onshape/std/curveGeometry.fs", version : "951.0");
+import(path : "onshape/std/drafttype.gen.fs", version : "951.0");
+import(path : "onshape/std/evaluate.fs", version : "951.0");
+import(path : "onshape/std/feature.fs", version : "951.0");
+import(path : "onshape/std/manipulator.fs", version : "951.0");
+import(path : "onshape/std/topologyUtils.fs", version : "951.0");
+import(path : "onshape/std/valueBounds.fs", version : "951.0");
+import(path : "onshape/std/vector.fs", version : "951.0");
 
 /**
  * Types of drafts available for the draft feature.
--- tabReferences.js
+++ tabReferences.js
@@ -1,7 +1,7 @@
-FeatureScript 9999; /* Automatically generated version */
-export import(path : "onshape/std/partstudioitemtype.gen.fs", version : "");
+FeatureScript 951; /* Automatically generated version */
+export import(path : "onshape/std/partstudioitemtype.gen.fs", version : "951.0");
 
-import(path : "onshape/std/query.fs", version : "");
+import(path : "onshape/std/query.fs", version : "951.0");
 
 /**
  * The value of a Part Studio reference parameter, specifying user-selected parts or other bodies from another
