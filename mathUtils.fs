b650e01e5254af650644365514c354c90485e6c8
mbartlett21 <mjmouse9999@gmail.com>
2019-03-01 18:43:15 +0000

    1024.0

--- mathUtils.fs
+++ mathUtils.fs
@@ -6,9 +6,10 @@ FeatureScript 9999; /* Automatically generated version */
 /**
  * This module imports the `math`, `matrix`, `transform`, and `vector` modules.
  * It is designed to be imported instead of the `geometry` module in Feature
- * Studios where only math (not higher level modelling functionality) is needed.
+ * Studios where only math (not higher-level modeling functionality) is needed.
  */
 export import(path : "onshape/std/math.fs", version : "");
 export import(path : "onshape/std/matrix.fs", version : "");
+export import(path : "onshape/std/matrixWithUnits.fs", version : "");
 export import(path : "onshape/std/transform.fs", version : "");
 export import(path : "onshape/std/vector.fs", version : "");
