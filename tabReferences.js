f9ed10361bfdd6dd61054679571667553f3b05f7
mbartlett21 <mjmouse9999@gmail.com>
2019-07-08 20:22:33 +0000

    1112.0

--- tabReferences.js
+++ tabReferences.js
@@ -1,7 +1,7 @@
-FeatureScript 9999; /* Automatically generated version */
-export import(path : "onshape/std/partstudioitemtype.gen.fs", version : "");
+FeatureScript 1112; /* Automatically generated version */
+export import(path : "onshape/std/partstudioitemtype.gen.fs", version : "1112.0");
 
-import(path : "onshape/std/query.fs", version : "");
+import(path : "onshape/std/query.fs", version : "1112.0");
 
 /**
  * The value of a Part Studio reference parameter, specifying user-selected parts or other bodies from another
