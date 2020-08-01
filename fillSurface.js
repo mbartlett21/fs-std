35f7f7d18989d1af5561384fea72fd4897bcc765
mbartlett21 <mjmouse9999@gmail.com>
2020-08-01 00:00:46 +0000

    1337.0 normalise

--- fillSurface.js
+++ fillSurface.js
@@ -1,19 +1,19 @@
-FeatureScript 1337; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
 
-export import(path : "onshape/std/geometriccontinuity.gen.fs", version : "1337.0");
-export import(path : "onshape/std/tool.fs", version : "1337.0");
+export import(path : "onshape/std/geometriccontinuity.gen.fs", version : "");
+export import(path : "onshape/std/tool.fs", version : "");
 
-import(path : "onshape/std/boolean.fs", version : "1337.0");
-import(path : "onshape/std/containers.fs", version : "1337.0");
-import(path : "onshape/std/feature.fs", version : "1337.0");
-import(path : "onshape/std/query.fs", version : "1337.0");
-import(path : "onshape/std/topologyUtils.fs", version : "1337.0");
-import(path : "onshape/std/transform.fs", version : "1337.0");
-import(path : "onshape/std/units.fs", version : "1337.0");
-import(path : "onshape/std/valueBounds.fs", version : "1337.0");
+import(path : "onshape/std/boolean.fs", version : "");
+import(path : "onshape/std/containers.fs", version : "");
+import(path : "onshape/std/feature.fs", version : "");
+import(path : "onshape/std/query.fs", version : "");
+import(path : "onshape/std/topologyUtils.fs", version : "");
+import(path : "onshape/std/transform.fs", version : "");
+import(path : "onshape/std/units.fs", version : "");
+import(path : "onshape/std/valueBounds.fs", version : "");
 
 /**
  * @internal
