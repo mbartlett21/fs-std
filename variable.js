658654c5c97fb58bacbd1acc72a37f7f17be2152
mbartlett21 <mjmouse9999@gmail.com>
2020-10-27 21:27:55 +0000

    1389.0 normalise

--- variable.js
+++ variable.js
@@ -1,19 +1,19 @@
-FeatureScript 1389; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
 
 // Imports used in interface
-export import(path : "onshape/std/query.fs", version : "1389.0");
+export import(path : "onshape/std/query.fs", version : "");
 
 // Imports used internally
-import(path : "onshape/std/containers.fs", version : "1389.0");
-import(path : "onshape/std/evaluate.fs", version : "1389.0");
-import(path : "onshape/std/feature.fs", version : "1389.0");
-import(path : "onshape/std/string.fs", version : "1389.0");
-import(path : "onshape/std/tool.fs", version : "1389.0");
-import(path : "onshape/std/valueBounds.fs", version : "1389.0");
-import(path : "onshape/std/manipulator.fs", version : "1389.0");
+import(path : "onshape/std/containers.fs", version : "");
+import(path : "onshape/std/evaluate.fs", version : "");
+import(path : "onshape/std/feature.fs", version : "");
+import(path : "onshape/std/string.fs", version : "");
+import(path : "onshape/std/tool.fs", version : "");
+import(path : "onshape/std/valueBounds.fs", version : "");
+import(path : "onshape/std/manipulator.fs", version : "");
 
 /**
  * Specifies the type of values `assignVariable` is allowed to set.
