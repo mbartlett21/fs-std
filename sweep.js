281ce7b19abae1c1b83e042778e21df1269f7b11
mbartlett21 <mjmouse9999@gmail.com>
2021-03-29 23:36:22 +0000

    1483.0 normalise

--- sweep.js
+++ sweep.js
@@ -1,20 +1,20 @@
-FeatureScript 1483; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
 
 // Imports used in interface
-export import(path : "onshape/std/query.fs", version : "1483.0");
-export import(path : "onshape/std/tool.fs", version : "1483.0");
+export import(path : "onshape/std/query.fs", version : "");
+export import(path : "onshape/std/tool.fs", version : "");
 
 // Imports used internally
-import(path : "onshape/std/boolean.fs", version : "1483.0");
-import(path : "onshape/std/booleanHeuristics.fs", version : "1483.0");
-import(path : "onshape/std/containers.fs", version : "1483.0");
-import(path : "onshape/std/evaluate.fs", version : "1483.0");
-import(path : "onshape/std/topologyUtils.fs", version : "1483.0");
-import(path : "onshape/std/transform.fs", version : "1483.0");
-import(path : "onshape/std/feature.fs", version : "1483.0");
+import(path : "onshape/std/boolean.fs", version : "");
+import(path : "onshape/std/booleanHeuristics.fs", version : "");
+import(path : "onshape/std/containers.fs", version : "");
+import(path : "onshape/std/evaluate.fs", version : "");
+import(path : "onshape/std/topologyUtils.fs", version : "");
+import(path : "onshape/std/transform.fs", version : "");
+import(path : "onshape/std/feature.fs", version : "");
 
 /**
  * Feature performing an [opSweep], followed by an [opBoolean]. For simple sweeps, prefer using
