ad984e35796c372261983a171e7521e5e5abe5a6
mbartlett21 <mjmouse9999@gmail.com>
2021-10-01 17:42:00 +0000

    1605.0 normalise

--- wrap.js
+++ wrap.js
@@ -1,31 +1,31 @@
-FeatureScript 1605; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
 
 // Imports used in interface
-export import(path : "onshape/std/query.fs", version : "1605.0");
-export import(path : "onshape/std/tool.fs", version : "1605.0");
-export import(path : "onshape/std/wraptype.gen.fs", version : "1605.0");
+export import(path : "onshape/std/query.fs", version : "");
+export import(path : "onshape/std/tool.fs", version : "");
+export import(path : "onshape/std/wraptype.gen.fs", version : "");
 
 // Features using manipulators must export manipulator.fs
-export import(path : "onshape/std/manipulator.fs", version : "1605.0");
+export import(path : "onshape/std/manipulator.fs", version : "");
 
 // Imports used internally
-import(path : "onshape/std/boolean.fs", version : "1605.0");
-import(path : "onshape/std/booleanHeuristics.fs", version : "1605.0");
-import(path : "onshape/std/box.fs", version : "1605.0");
-import(path : "onshape/std/containers.fs", version : "1605.0");
-import(path : "onshape/std/coordSystem.fs", version : "1605.0");
-import(path : "onshape/std/curveGeometry.fs", version : "1605.0");
-import(path : "onshape/std/evaluate.fs", version : "1605.0");
-import(path : "onshape/std/feature.fs", version : "1605.0");
-import(path : "onshape/std/sheetMetalUtils.fs", version : "1605.0");
-import(path : "onshape/std/surfaceGeometry.fs", version : "1605.0");
-import(path : "onshape/std/transform.fs", version : "1605.0");
-import(path : "onshape/std/valueBounds.fs", version : "1605.0");
-import(path : "onshape/std/vector.fs", version : "1605.0");
-import(path : "onshape/std/wrapSurface.fs", version : "1605.0");
+import(path : "onshape/std/boolean.fs", version : "");
+import(path : "onshape/std/booleanHeuristics.fs", version : "");
+import(path : "onshape/std/box.fs", version : "");
+import(path : "onshape/std/containers.fs", version : "");
+import(path : "onshape/std/coordSystem.fs", version : "");
+import(path : "onshape/std/curveGeometry.fs", version : "");
+import(path : "onshape/std/evaluate.fs", version : "");
+import(path : "onshape/std/feature.fs", version : "");
+import(path : "onshape/std/sheetMetalUtils.fs", version : "");
+import(path : "onshape/std/surfaceGeometry.fs", version : "");
+import(path : "onshape/std/transform.fs", version : "");
+import(path : "onshape/std/valueBounds.fs", version : "");
+import(path : "onshape/std/vector.fs", version : "");
+import(path : "onshape/std/wrapSurface.fs", version : "");
 
 /**
  * Defines what type of output the Wrap feature should produce.
