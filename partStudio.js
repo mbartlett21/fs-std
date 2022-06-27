6a45b2ca6c2678fc35ce9bbcc643840ee85065ce
mbartlett21 <mjmouse9999@gmail.com>
2022-06-27 23:25:15 +0000

    1793.0 normalise

--- partStudio.js
+++ partStudio.js
@@ -1,9 +1,9 @@
-FeatureScript 1793; /* Automatically generated version */
-import(path : "onshape/std/context.fs", version : "1793.0");
-import(path : "onshape/std/defaultFeatures.fs", version : "1793.0");
+FeatureScript 9999; /* Automatically generated version */
+import(path : "onshape/std/context.fs", version : "");
+import(path : "onshape/std/defaultFeatures.fs", version : "");
 
-import(path : "onshape/std/containers.fs", version : "1793.0");
-import(path : "onshape/std/units.fs", version : "1793.0");
+import(path : "onshape/std/containers.fs", version : "");
+import(path : "onshape/std/units.fs", version : "");
 
 /** @internal */
 export function definePartStudio(partStudio is function, defaultLengthUnit is ValueWithUnits, defaults is map) returns function
