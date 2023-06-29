432064057fcaf53b415d8f1962e5ed87a1583c12
mbartlett21 <mjmouse9999@gmail.com>
2023-06-29 22:19:41 +0000

    2075.0

--- faceIntersection.js
+++ faceIntersection.js
@@ -1,5 +1,5 @@
-FeatureScript 9999; /* Automatically generated version */
-import(path : "onshape/std/feature.fs", version : "");
+FeatureScript 2075; /* Automatically generated version */
+import(path : "onshape/std/feature.fs", version : "2075.0");
 
 /**
  *  Creates curves where two faces intersect.
--- lookupTablePath.js
+++ lookupTablePath.js
@@ -1,7 +1,7 @@
-FeatureScript 9999; /* Automatically generated version */
-import(path : "onshape/std/math.fs", version : "");
-import(path : "onshape/std/string.fs", version : "");
-import(path : "onshape/std/units.fs", version : "");
+FeatureScript 2075; /* Automatically generated version */
+import(path : "onshape/std/math.fs", version : "2075.0");
+import(path : "onshape/std/string.fs", version : "2075.0");
+import(path : "onshape/std/units.fs", version : "2075.0");
 
 /**
  * A `LookupTablePath` identifies a map of keys into a multi-level table.
