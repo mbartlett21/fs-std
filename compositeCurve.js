8132cba10fbe07e6b84c6b5bbcc09fbb00592bcc
mbartlett21 <mjmouse9999@gmail.com>
2023-09-01 19:32:48 +0000

    2130.0

--- compositeCurve.js
+++ compositeCurve.js
@@ -1,6 +1,6 @@
-FeatureScript 9999; /* Automatically generated version */
-import(path : "onshape/std/feature.fs", version : "");
-import(path : "onshape/std/topologyUtils.fs", version : "");
+FeatureScript 2130; /* Automatically generated version */
+import(path : "onshape/std/feature.fs", version : "2130.0");
+import(path : "onshape/std/topologyUtils.fs", version : "2130.0");
 
 /**
  * Creates one or more Curves that are a combination of edges from various sources, be they parts, surfaces,
