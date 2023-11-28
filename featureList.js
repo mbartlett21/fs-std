5aac7c689bb38b415f895c73db0e0775171490e1
mbartlett21 <mjmouse9999@gmail.com>
2023-11-28 23:19:22 +0000

    2207.0

--- featureList.js
+++ featureList.js
@@ -1,11 +1,11 @@
-FeatureScript 9999; /* Automatically generated version */
+FeatureScript 2207; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
 
 /** Support functions for feature lists (as used for featurePattern) */
 
-import(path : "onshape/std/context.fs", version : "");
+import(path : "onshape/std/context.fs", version : "2207.0");
 
 /**
  * Parameter type for inputting a list of features, stored as a map from
@@ -41,4 +41,13 @@ export function featureList(features is map) returns FeatureList
 export function valuesSortedById(context is Context, idToValue is map) returns array
 {
     return @valuesSortedById(context, idToValue);
+}
+
+/**
+ * @internal
+ * Returns true if any features listed in idToValue is a sketch.
+ */
+export function containsSketch(context is Context, idToValue is map)
+{
+    return @containsSketch(context, idToValue);
 }
