7b89f1e0c22032cecde73a9b5c9209fd7b53aeb1
mbartlett21 <mjmouse9999@gmail.com>
2024-04-05 19:21:18 +0000

    2321.0

--- faceIntersection.js
+++ faceIntersection.js
@@ -1,5 +1,5 @@
-FeatureScript 9999; /* Automatically generated version */
-import(path : "onshape/std/feature.fs", version : "");
+FeatureScript 2321; /* Automatically generated version */
+import(path : "onshape/std/feature.fs", version : "2321.0");
 
 /**
  *  Creates curves where two faces intersect.
