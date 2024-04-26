738c8a5a5abcbe29d1436903b1ef2b52d6ec3d47
mbartlett21 <mjmouse9999@gmail.com>
2024-04-26 20:06:09 +0000

    2345.0

--- sidegeometryrule.gen.js
+++ sidegeometryrule.gen.js
@@ -1,4 +1,4 @@
-FeatureScript 9999; /* Automatically generated version */
+FeatureScript 2345; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present PTC Inc.
@@ -9,6 +9,8 @@ FeatureScript 9999; /* Automatically generated version */
 export enum SideGeometryRule
 {
     annotation {"Name" : "Revolved"}
-    REVOLVED
+    REVOLVED,
+    annotation {"Name" : "Supplied"}
+    SUPPLIED
 }
 
