734113a0014d50f197fd5289157978f553f3801a
mbartlett21 <mjmouse9999@gmail.com>
2024-05-17 19:39:05 +0000

    2368.0

--- manipulatortype.gen.fs
+++ manipulatortype.gen.fs
@@ -19,6 +19,7 @@ FeatureScript 9999; /* Automatically generated version */
  *         example.
  * @value POINTS       : A series of points which can be selected one at a time.
  * @value TOGGLE_POINTS : A series of points which can each be selected individually.
+ * @value TRIAD_FULL: A triad of perpendicular arrows, planar and angular manipulators.
  */
 export enum ManipulatorType
 {
@@ -27,6 +28,7 @@ export enum ManipulatorType
     ANGULAR,
     FLIP,
     POINTS,
-    TOGGLE_POINTS
+    TOGGLE_POINTS,
+    TRIAD_FULL
 }
 
