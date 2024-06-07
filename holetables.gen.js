b781e62e2df938be30a24da0e50620ede68fbbb7
mbartlett21 <mjmouse9999@gmail.com>
2024-06-07 19:50:31 +0000

    2384.0

--- holetables.gen.js
+++ holetables.gen.js
@@ -1,8 +1,8 @@
-FeatureScript 9999; /* Automatically generated version */
+FeatureScript 2384; /* Automatically generated version */
 /* Automatically generated file -- DO NOT EDIT */
 
-import(path : "onshape/std/units.fs", version : "");
-import(path : "onshape/std/lookupTablePath.fs", version : "");
+import(path : "onshape/std/units.fs", version : "2384.0");
+import(path : "onshape/std/lookupTablePath.fs", version : "2384.0");
 
 const ANSI_TaperedPipeTapHoleTable = {
     "name" : "size",
