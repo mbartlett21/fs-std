fd1907ccc53c4cf59f80c7bca96e333a2d64da8d
mbartlett21 <mjmouse9999@gmail.com>
2024-08-12 19:11:46 +0000

    2433.0 normalise

--- computedPartProperty.js
+++ computedPartProperty.js
@@ -1,12 +1,12 @@
-FeatureScript 2433; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2020-Present Onshape Inc.
 
-export import(path : "onshape/std/context.fs", version : "2433.0");
-export import(path : "onshape/std/feature.fs", version : "2433.0");
-export import(path : "onshape/std/query.fs", version : "2433.0");
-export import(path : "onshape/std/units.fs", version : "2433.0");
+export import(path : "onshape/std/context.fs", version : "");
+export import(path : "onshape/std/feature.fs", version : "");
+export import(path : "onshape/std/query.fs", version : "");
+export import(path : "onshape/std/units.fs", version : "");
 
 /**
  *
