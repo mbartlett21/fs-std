f6476519b9d9f46b81845f4d3ae26d1838a1f280
mbartlett21 <mjmouse9999@gmail.com>
2024-11-05 08:52:55 +1000

    0.3.0

---
+++ dimensionalignment.gen.js
@@ -0,0 +1,9 @@
+/* Automatically generated file -- DO NOT EDIT */
+
+export enum DimensionAlignment
+{
+    UNSPECIFIED,
+    ALIGNED,
+    ANTI_ALIGNED
+}
+
---
+++ dimensionhalfspace.gen.js
@@ -0,0 +1,9 @@
+/* Automatically generated file -- DO NOT EDIT */
+
+export enum DimensionHalfSpace
+{
+    UNSPECIFIED,
+    LEFT,
+    RIGHT
+}
+
