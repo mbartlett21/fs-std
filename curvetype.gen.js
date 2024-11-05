d9085f5cb639ddc377829e9a524c4ba1757101c1
mbartlett21 <mjmouse9999@gmail.com>
2024-11-05 13:20:31 +1000

    50.0

---
+++ curvetype.gen.js
@@ -0,0 +1,9 @@
+/* Automatically generated file -- DO NOT EDIT */
+
+export enum CurveType
+{
+    CIRCLE,
+    LINE,
+    OTHER
+}
+
---
+++ surfacetype.gen.js
@@ -0,0 +1,12 @@
+/* Automatically generated file -- DO NOT EDIT */
+
+export enum SurfaceType
+{
+    PLANE,
+    CYLINDER,
+    CONE,
+    SPHERE,
+    TORUS,
+    OTHER
+}
+
