d9c352dd94fd817b0342e6ec576e6d2a98276d27
mbartlett21 <mjmouse9999@gmail.com>
2024-11-06 21:14:51 +1000

    369.0 normalise

--- bendoptions.js
+++ bendoptions.js
@@ -1,5 +1,5 @@
-FeatureScript 369; /* Automatically generated version */
-export import(path : "onshape/std/units.fs", version : "369.0");
+FeatureScript 9999; /* Automatically generated version */
+export import(path : "onshape/std/units.fs", version : "");
 
 /**
  * @internal
