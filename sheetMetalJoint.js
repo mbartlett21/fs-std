88d932623d8808a328c7df0151dc3b4b33665919
mbartlett21 <mjmouse9999@gmail.com>
2024-11-06 22:17:24 +1000

    455.0

--- sheetMetalJoint.js
+++ sheetMetalJoint.js
@@ -1,4 +1,4 @@
-FeatureScript 9999; /* Automatically generated version */
+FeatureScript 455; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
@@ -10,20 +10,20 @@ FeatureScript 9999; /* Automatically generated version */
  */
 
 
-export import(path : "onshape/std/smjointtype.gen.fs", version : "");
-export import(path : "onshape/std/smjointstyle.gen.fs", version : "");
+export import(path : "onshape/std/smjointtype.gen.fs", version : "455.0");
+export import(path : "onshape/std/smjointstyle.gen.fs", version : "455.0");
 
-import(path : "onshape/std/sheetMetalAttribute.fs", version : "");
-import(path : "onshape/std/sheetMetalUtils.fs", version : "");
-import(path : "onshape/std/feature.fs", version : "");
-import(path : "onshape/std/valueBounds.fs", version : "");
-import(path : "onshape/std/containers.fs", version : "");
-import(path : "onshape/std/attributes.fs", version : "");
-import(path : "onshape/std/evaluate.fs", version : "");
-import(path : "onshape/std/surfaceGeometry.fs", version : "");
-import(path : "onshape/std/math.fs", version : "");
-import(path : "onshape/std/modifyFillet.fs", version : "");
-import(path : "onshape/std/string.fs", version : "");
+import(path : "onshape/std/sheetMetalAttribute.fs", version : "455.0");
+import(path : "onshape/std/sheetMetalUtils.fs", version : "455.0");
+import(path : "onshape/std/feature.fs", version : "455.0");
+import(path : "onshape/std/valueBounds.fs", version : "455.0");
+import(path : "onshape/std/containers.fs", version : "455.0");
+import(path : "onshape/std/attributes.fs", version : "455.0");
+import(path : "onshape/std/evaluate.fs", version : "455.0");
+import(path : "onshape/std/surfaceGeometry.fs", version : "455.0");
+import(path : "onshape/std/math.fs", version : "455.0");
+import(path : "onshape/std/modifyFillet.fs", version : "455.0");
+import(path : "onshape/std/string.fs", version : "455.0");
 
 /**
  * @internal
