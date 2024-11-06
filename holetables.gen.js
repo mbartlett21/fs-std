a93b465c9d728ea9b84bc0849772c1510bfcf151
mbartlett21 <mjmouse9999@gmail.com>
2024-11-07 00:50:21 +1000

    593.0 normalise

--- holetables.gen.js
+++ holetables.gen.js
@@ -1,8 +1,8 @@
-FeatureScript 593; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 /* Automatically generated file -- DO NOT EDIT */
 
-import(path : "onshape/std/units.fs", version : "593.0");
-import(path : "onshape/std/lookupTablePath.fs", version : "593.0");
+import(path : "onshape/std/units.fs", version : "");
+import(path : "onshape/std/lookupTablePath.fs", version : "");
 
 const ANSI_drillTable = {
     "name" : "size",
