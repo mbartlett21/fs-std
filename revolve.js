3c5b9c583b06bc06b2c85fe90a3a20f81d36c2fe
mbartlett21 <mjmouse9999@gmail.com>
2024-11-06 21:06:51 +1000

    355.0 normalise

--- revolve.js
+++ revolve.js
@@ -1,21 +1,21 @@
-FeatureScript 355; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 // This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
 // See the LICENSE tab for the license text.
 // Copyright (c) 2013-Present Onshape Inc.
 
 // Imports used in interface
-export import(path : "onshape/std/tool.fs", version : "355.0");
+export import(path : "onshape/std/tool.fs", version : "");
 
 // Features using manipulators must export manipulator.fs
-export import(path : "onshape/std/manipulator.fs", version : "355.0");
+export import(path : "onshape/std/manipulator.fs", version : "");
 
 // Imports used internally
-import(path : "onshape/std/boolean.fs", version : "355.0");
-import(path : "onshape/std/booleanHeuristics.fs", version : "355.0");
-import(path : "onshape/std/evaluate.fs", version : "355.0");
-import(path : "onshape/std/feature.fs", version : "355.0");
-import(path : "onshape/std/mathUtils.fs", version : "355.0");
-import(path : "onshape/std/valueBounds.fs", version : "355.0");
+import(path : "onshape/std/boolean.fs", version : "");
+import(path : "onshape/std/booleanHeuristics.fs", version : "");
+import(path : "onshape/std/evaluate.fs", version : "");
+import(path : "onshape/std/feature.fs", version : "");
+import(path : "onshape/std/mathUtils.fs", version : "");
+import(path : "onshape/std/valueBounds.fs", version : "");
 
 /**
  * Specifies how a revolve's end condition should be defined.
