a816ab7370c4bb9efec26172b263e3d5a305e93c
mbartlett21 <mjmouse9999@gmail.com>
2024-11-07 00:25:10 +1000

    543.0 normalise

--- lookupTablePath.js
+++ lookupTablePath.js
@@ -1,7 +1,7 @@
-FeatureScript 543; /* Automatically generated version */
-import(path : "onshape/std/math.fs", version : "543.0");
-import(path : "onshape/std/string.fs", version : "543.0");
-import(path : "onshape/std/units.fs", version : "543.0");
+FeatureScript 9999; /* Automatically generated version */
+import(path : "onshape/std/math.fs", version : "");
+import(path : "onshape/std/string.fs", version : "");
+import(path : "onshape/std/units.fs", version : "");
 
 /**
  * A `LookupTablePath` identifies a map of keys into a multi-level table.
