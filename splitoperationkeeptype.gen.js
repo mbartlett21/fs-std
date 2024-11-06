43ae4069547322ecef856a789efa3e920ccdf170
mbartlett21 <mjmouse9999@gmail.com>
2024-11-06 20:39:59 +1000

    328.0

--- splitoperationkeeptype.gen.js
+++ splitoperationkeeptype.gen.js
@@ -6,14 +6,13 @@ FeatureScript 9999; /* Automatically generated version */
 /* Automatically generated file -- DO NOT EDIT */
 
 /** TODO: documentation */
-export enum MeasureLabelTypeEnum
+export enum SplitOperationKeepType
 {
-    EMPTY,
-    CENTER,
-    START_POINT,
-    END_POINT,
-    ELLIPSE_CENTER,
-    MAX_RADIUS_POINT,
-    MIN_RADIUS_POINT
+    annotation {"Name" : "Keep all"}
+    KEEP_ALL,
+    annotation {"Name" : "Keep front"}
+    KEEP_FRONT,
+    annotation {"Name" : "Keep back"}
+    KEEP_BACK
 }
 
