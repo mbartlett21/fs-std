a29574e911595078f1898c601203f3fe1e6992f3
mbartlett21 <mjmouse9999@gmail.com>
2024-11-06 21:43:27 +1000

    392.0 normalise

--- holetables.gen.js
+++ holetables.gen.js
@@ -1,8 +1,8 @@
-FeatureScript 392; /* Automatically generated version */
+FeatureScript 9999; /* Automatically generated version */
 /* Automatically generated file -- DO NOT EDIT */
 
-import(path : "onshape/std/units.fs", version : "392.0");
-import(path : "onshape/std/lookupTablePath.fs", version : "392.0");
+import(path : "onshape/std/units.fs", version : "");
+import(path : "onshape/std/lookupTablePath.fs", version : "");
 
 const ANSI_drillTable = {
     "name" : "size",
--- partStudio.js
+++ partStudio.js
@@ -1,9 +1,9 @@
-FeatureScript 392; /* Automatically generated version */
-import(path : "onshape/std/context.fs", version : "392.0");
-import(path : "onshape/std/defaultFeatures.fs", version : "392.0");
+FeatureScript 9999; /* Automatically generated version */
+import(path : "onshape/std/context.fs", version : "");
+import(path : "onshape/std/defaultFeatures.fs", version : "");
 
-import(path : "onshape/std/containers.fs", version : "392.0");
-import(path : "onshape/std/units.fs", version : "392.0");
+import(path : "onshape/std/containers.fs", version : "");
+import(path : "onshape/std/units.fs", version : "");
 
 /** @internal */
 export function definePartStudio(partStudio is function, defaultLengthUnit is ValueWithUnits, defaults is map) returns function
