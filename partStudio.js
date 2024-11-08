b8ffd9723cc59495f46c5b8ba7dcb877e78502bd
mbartlett21 <mjmouse9999@gmail.com>
2024-11-08 12:40:25 +1000

    701.0

--- partStudio.js
+++ partStudio.js
@@ -1,9 +1,9 @@
-FeatureScript 9999; /* Automatically generated version */
-import(path : "onshape/std/context.fs", version : "");
-import(path : "onshape/std/defaultFeatures.fs", version : "");
+FeatureScript 701; /* Automatically generated version */
+import(path : "onshape/std/context.fs", version : "701.0");
+import(path : "onshape/std/defaultFeatures.fs", version : "701.0");
 
-import(path : "onshape/std/containers.fs", version : "");
-import(path : "onshape/std/units.fs", version : "");
+import(path : "onshape/std/containers.fs", version : "701.0");
+import(path : "onshape/std/units.fs", version : "701.0");
 
 /** @internal */
 export function definePartStudio(partStudio is function, defaultLengthUnit is ValueWithUnits, defaults is map) returns function
