8de8c3c3c78971a2fa0d8585a082080cbd722ad0
mbartlett21 <mjmouse9999@gmail.com>
2025-01-10 19:43:25 +0000

    2559.0

---
+++ gtolconstrainttype.gen.fs
@@ -0,0 +1,17 @@
+FeatureScript 9999; /* Automatically generated version */
+// This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
+// See the LICENSE tab for the license text.
+// Copyright (c) 2013-Present PTC Inc.
+
+/* Automatically generated file -- DO NOT EDIT */
+
+/** @internal */
+export enum GTolConstraintType
+{
+    TRUE_POSITION,
+    PARALLELISM,
+    PERPENDICULARITY,
+    PROFILE_SURFACE,
+    TOTAL_RUNOUT
+}
+
