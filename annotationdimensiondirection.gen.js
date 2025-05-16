018754e1effcefdc9c6620736d49fcd13627e84b
mbartlett21 <mjmouse9999@gmail.com>
2025-05-16 17:48:08 +0000

    2656.0

--- annotationdimensiondirection.gen.js
+++ annotationdimensiondirection.gen.js
@@ -5,16 +5,14 @@ FeatureScript 2656; /* Automatically generated version */
 
 /* Automatically generated file -- DO NOT EDIT */
 
-/** See [opChamfer]. */
-export enum ChamferType
+/**
+ * Used to specify the measurement direction for annotation dimensions.
+ */
+export enum AnnotationDimensionDirection
 {
-    annotation {"Name" : "Equal distance"}
-    EQUAL_OFFSETS,
-    annotation {"Name" : "Two distances"}
-    TWO_OFFSETS,
-    annotation {"Name" : "Distance and angle"}
-    OFFSET_ANGLE,
-    annotation {"Hidden" : true}
-    RAW_OFFSET
+    DEFAULT,
+    GLOBAL_X,
+    GLOBAL_Y,
+    SPECIFIC
 }
 
--- faceIntersection.js
+++ faceIntersection.js
@@ -1,6 +1,6 @@
-FeatureScript 9999; /* Automatically generated version */
-import(path : "onshape/std/feature.fs", version : "");
-import(path : "onshape/std/approximationUtils.fs", version : "");
+FeatureScript 2656; /* Automatically generated version */
+import(path : "onshape/std/feature.fs", version : "2656.0");
+import(path : "onshape/std/approximationUtils.fs", version : "2656.0");
 
 /**
  *  Creates curves where two faces intersect.
