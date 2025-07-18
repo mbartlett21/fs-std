21667c44f2c2135b09694bdaec670d8a597c729e
mbartlett21 <mjmouse9999@gmail.com>
2025-07-18 17:16:59 +0000

    2716.0

---
+++ weldstandard.gen.fs
@@ -0,0 +1,16 @@
+FeatureScript 9999; /* Automatically generated version */
+// This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
+// See the LICENSE tab for the license text.
+// Copyright (c) 2013-Present Onshape Inc.
+
+/* Automatically generated file -- DO NOT EDIT */
+
+/**
+ * Enum to switch the weld symbol representation between ANSI and ISO standard.
+ */
+export enum WeldStandard
+{
+    ANSI,
+    ISO
+}
+
