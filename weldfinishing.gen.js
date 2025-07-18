867f6e33b640e2407944f487b792b2f4c2364137
mbartlett21 <mjmouse9999@gmail.com>
2025-07-18 17:16:59 +0000

    2716.0

---
+++ weldfinishing.gen.js
@@ -0,0 +1,21 @@
+FeatureScript 9999; /* Automatically generated version */
+// This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
+// See the LICENSE tab for the license text.
+// Copyright (c) 2013-Present Onshape Inc.
+
+/* Automatically generated file -- DO NOT EDIT */
+
+/**
+ * Enum to determine the weld finishing in ANSI standard.
+ */
+export enum WeldFinishing
+{
+    NONE,
+    CHIPPING,
+    GRINDING,
+    HAMMERING,
+    MACHINING,
+    ROLLING,
+    UNSPECIFIED
+}
+
