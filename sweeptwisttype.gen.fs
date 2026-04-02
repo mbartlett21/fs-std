63ba979ae9e01bc18a3eed0fb9628ebae77035b3
mbartlett21 <mjmouse9999@gmail.com>
2026-04-02 16:37:33 +1000

    2892.0
    2026-02-20 19:22

---
+++ sweeptwisttype.gen.fs
@@ -0,0 +1,20 @@
+FeatureScript 9999; /* Automatically generated version */
+// This module is part of the FeatureScript Standard Library and is distributed under the MIT License.
+// See the LICENSE tab for the license text.
+// Copyright (c) 2013-Present Onshape Inc.
+
+/* Automatically generated file -- DO NOT EDIT */
+
+/**
+ * Specifies twist type for sweep.
+ */
+export enum SweepTwistType
+{
+    annotation {"Name" : "Turns"}
+    TURNS,
+    annotation {"Name" : "Angle"}
+    ANGLE,
+    annotation {"Name" : "Pitch"}
+    PITCH
+}
+
